11e9e476d914fe39558c010ffb41534efc312d0e
hfhoffman1144 <hfh160130@utdallas.edu>
2022-01-04 14:06:56 -0600

    plots

--- flaskapp/api_routes.py
+++ flaskapp/api_routes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from flask import Blueprint, request
-from services.model_utils import predict_hf
+from services.model_utils import predict_hf, plot_shap_values
 
 bp1 = Blueprint('main', __name__, url_prefix='/main')
 
@@ -12,5 +12,7 @@ def make_prediction():
     pred = predict_hf(form_df)
     pred_class_1 = pred[0][1]
 
-    return {'pred':round(pred_class_1, 2)}
+    plot = plot_shap_values(form_df)
+
+    return {'pred':round(pred_class_1, 2), 'plot':plot}
 
--- flaskapp/services/model_utils.py
+++ flaskapp/services/model_utils.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
-from catboost import CatBoostClassifier
+from catboost import CatBoostClassifier, Pool
+import plotly.graph_objects as go
 
 # Global variable that stores trained model instance
 MODEL = CatBoostClassifier().load_model('models/heart_disease_model_2021-12-28')
@@ -19,5 +21,39 @@ def predict_hf(data:pd.DataFrame):
     data_predict = data_predict[MODEL.feature_names_]
 
     return MODEL.predict_proba(data_predict)
+
+def get_shap_df(data:pd.DataFrame):
+
+    # Make sure column names are correct
+    data_predict = data.rename(display_to_model, axis=1)
+
+    # Make sure columns are in the correct order
+    data_predict = data_predict[MODEL.feature_names_]
+
+    data_pool = Pool(data_predict, cat_features=MODEL.get_cat_feature_indices())
+
+    shap_values = MODEL.get_feature_importance(data_pool, type='ShapValues')
+    
+    shap_values = shap_values[:,:-1].reshape(shap_values.shape[0], len(MODEL.feature_names_))
+    shap_df = pd.DataFrame(shap_values, columns=MODEL.feature_names_).T
+    shap_df.columns = ['feature']
+    shap_df['AbsVal'] = np.abs(shap_df['feature'])
+    shap_df.sort_values('AbsVal', ascending=False, inplace=True)
+
+    return shap_df
+
+def plot_shap_values(data:pd.DataFrame):
+
+    shap_df = get_shap_df(data)
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=shap_df.index, y=shap_df.feature))
+    fig.update_layout(title='Patient Risk Factors')
+
+    return fig.to_json()
+
+
+
+
 
 
